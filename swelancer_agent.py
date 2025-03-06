1098e84a5feb8edc9ba64d5d92924038ea111373
Alex Lourenco <alex.lourenco@mail.com>
2025-03-06 10:27:43 -0800

    Revert "Add support for extra_hosts in LocalConfig and LocalCluster"
    This reverts commit ce8fe81f2d47a05fa0d3952198d9c3967c094345.

--- swelancer_agent.py
+++ swelancer_agent.py
@@ -183,12 +183,7 @@ class SimpleAgentSolver(PythonCodingSolver):
     async def _start_computer(self, task: ComputerTask) -> AsyncGenerator[ComputerInterface, None]:
         # replace with LocalCluster semantics
 
-        alcatraz_env = task_to_alcatraz_config(task, LocalConfig(
-            pull_from_registry=False,
-            extra_hosts={
-                "host.docker.internal": "host-gateway"
-            }
-        ))
+        alcatraz_env = task_to_alcatraz_config(task, LocalConfig(pull_from_registry=False))
 
         async with alcatraz_env.build() as cluster:
             yield AlcatrazComputerInterface(cluster_value=cluster)
